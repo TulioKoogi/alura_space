2c8665184ed1471ca092631c92ed556ad414a2a4
Túlio <tulio-koogi@hotmail.com>
2024-08-22 16:52:39 -0300

    Criação do Templates

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(args):
-    return HttpResponse('<h1>Alura Space</h1><p>Seja bem vindo ao Espaço</p>')
+def index(request):
+    return render(request, 'index.html')
